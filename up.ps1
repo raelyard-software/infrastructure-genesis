9b825be442ef35529e8e0865abb0a1be15a94028
Dave Rael <dave@raelyard.com>
2024-05-18 12:27:49 -0600

    Make creation script idempotent
    such that it will create the local cluster if it does not already
    exist and not otherwise

--- up.ps1
+++ up.ps1
@@ -1,3 +1,12 @@
 #!/usr/bin/env pwsh
+$ErrorActionPreference = "Stop"
 
-kind create cluster --name genesis
+function EnsureGensisCluster {
+    $clusterName = "genesis"
+    $clusterStatus = kind get clusters | Select-String -Pattern $clusterName
+    if ($clusterStatus -eq $null) {
+        kind create cluster --name $clusterName
+    }
+}
+
+EnsureGensisCluster
