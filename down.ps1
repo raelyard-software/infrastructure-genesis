4dbba1a039efcd4830e471a5032538f3d9bd449c
Dave Rael <dave@raelyard.com>
2024-05-18 13:13:26 -0600

    Make deletion script idempotent
    such that it will not try to delete a cluster that doesn't exist

--- down.ps1
+++ down.ps1
@@ -1,3 +1,17 @@
 #!/usr/bin/env pwsh
 
-kind delete cluster --name genesis
+function TeardownGensisCluster {
+    $clusterName = "genesis"
+    $clusterStatus = kind get clusters | Select-String -Pattern $clusterName
+    if ($clusterStatus) {
+        try {
+            kind delete cluster --name genesis
+        }
+        catch {
+            # The first ateempt to delete the cluster may fail on trying to kill container - retry generally works
+            kind delete cluster --name genesis
+        }
+    }
+}
+
+TeardownGensisCluster
