bdd2bd3c4515ae5f4110bb1c4c932e297de28244
Dave Rael <dave@raelyard.com>
2024-05-19 13:35:46 -0600

    Make Azure Service Operator available
    by installing it via Helm into the cluster along with Cert Manager, which
    is necessary for a successful installation of Azure Service Operator
    
    Azure Service Operator is necessary in the cluster to be able to use
    Kubernetes resources to represent Azure Resources and bring htem into
    being

--- up.ps1
+++ up.ps1
@@ -4,9 +4,39 @@ $ErrorActionPreference = "Stop"
 function EnsureGensisCluster {
     $clusterName = "genesis"
     $clusterStatus = kind get clusters | Select-String -Pattern $clusterName
-    if ($clusterStatus -eq $null) {
+    if ($null -eq $clusterStatus) {
         kind create cluster --name $clusterName
     }
 }
 
+function EnsureCertManager {
+    $chartStatus = helm list -n cert-manager | Select-String -Pattern "cert-manager"
+    if ($null -ne $chartStatus) {
+        return
+    }
+    # check if repo already exists
+    $repoStatus = helm repo list | Select-String -Pattern "jetstack"
+    if ($null -eq $repoStatus) {
+        helm repo add jetstack https://charts.jetstack.io
+    }
+    helm repo update jetstack
+    helm upgrade --install cert-manager jetstack/cert-manager --create-namespace --namespace cert-manager ` --set installCRDs=true
+}
+
+function EnsureAzureServiceOperator {
+    $chartStatus = helm list -n azureserviceoperator-system | Select-String -Pattern "aso2"
+    if ($null -ne $chartStatus) {
+        return
+    }
+    $repoStatus = helm repo list | Select-String -Pattern "aso2"
+    if ($null -eq $repoStatus) {
+        helm repo add aso2 https://raw.githubusercontent.com/Azure/azure-service-operator/main/v2/charts
+    }
+    helm repo update aso2
+    helm upgrade --install aso2 aso2/azure-service-operator --create-namespace --namespace=azureserviceoperator-system `
+        --set crdPattern='resources.azure.com/*;containerservice.azure.com/*;keyvault.azure.com/*;managedidentity.azure.com/*;eventhub.azure.com/*'
+}
+
 EnsureGensisCluster
+EnsureCertManager
+EnsureAzureServiceOperator
